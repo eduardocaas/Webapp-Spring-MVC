936a38420e6f083fee2a07d0e0823a63d753b63f
eduardocaas <eduardofc.dev@gmail.com>
2023-04-15 12:15:11 -0300

    First commit

---
+++ src/main/java/com/efc/WebappSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.efc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/efc/WebappSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
