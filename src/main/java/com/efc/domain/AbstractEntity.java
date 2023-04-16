3495d41627bd14825729376498f3c55a39410a79
eduardocaas <eduardofc.dev@gmail.com>
2023-04-16 20:31:19 -0300

    Fix warning for serializable

--- src/main/java/com/efc/domain/AbstractEntity.java
+++ src/main/java/com/efc/domain/AbstractEntity.java
@@ -1,14 +1,50 @@
 package com.efc.domain;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
-public class AbstractEntity<ID extends Serializable> implements Serializable{
+@SuppressWarnings("serial")
+@MappedSuperclass
+public abstract class AbstractEntity<ID extends Serializable> implements Serializable{
 
-	private static final long serialVersionUID = 1L;
-
-	@Id
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private ID id;
+
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity<?> other = (AbstractEntity<?>) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "id=" + id;
+	}
+	
+	
 	
 }
