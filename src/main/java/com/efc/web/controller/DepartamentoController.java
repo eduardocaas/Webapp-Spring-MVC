017ca7fc0090e2ebe74934f7cbb1da860de13741
eduardocaas <eduardofc.dev@gmail.com>
2023-04-24 23:36:00 -0300

    Implemented Insert and Update service in controller

--- src/main/java/com/efc/web/controller/DepartamentoController.java
+++ src/main/java/com/efc/web/controller/DepartamentoController.java
@@ -1,21 +1,49 @@
 package com.efc.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.efc.domain.Departamento;
+import com.efc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("departamentos", service.findAll());
 		return "/departamento/lista";
 	}
 	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		this.service.save(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("departamento", service.findById(id));
+		return "/departamento/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Departamento departamento) {
+		this.service.update(departamento);
+		return "redirect:/departamentos/cadastrar";
+	}
 }
