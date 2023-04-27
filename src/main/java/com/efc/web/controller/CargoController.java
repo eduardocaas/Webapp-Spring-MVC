5cf65d02e0ef6dc87fd693fe50fc4f7fdfcf15c2
eduardocaas <eduardofc.dev@gmail.com>
2023-04-26 23:43:55 -0300

    Implemented controller for insert Cargo and list Departamentos

--- src/main/java/com/efc/web/controller/CargoController.java
+++ src/main/java/com/efc/web/controller/CargoController.java
@@ -1,21 +1,53 @@
 package com.efc.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.efc.domain.Cargo;
+import com.efc.domain.Departamento;
+import com.efc.service.CargoService;
+import com.efc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
+	@Autowired
+	private CargoService cargoService;
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.findAll());
 		return "/cargo/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.save(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departamentos") // Injeta os departamentos no model departamentos -> pode ser visto em cadastro.html (Cargo)
+	public List<Departamento> listaDepartamentos(){
+		return departamentoService.findAll();
+	}
+	
+	
 	
 }
